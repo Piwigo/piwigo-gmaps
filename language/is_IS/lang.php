aa97fc0fb9d41433d9029f462dd58ccde150a420
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-25 07:48:26 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['out of %d'] = 'af %d';
+$lang['show all photos around this location'] = 'sýna allar myndir í kringum þennan stað';
+$lang['Failed'] = 'Mistókst';
+$lang['Geotag'] = 'Landupplýsingamerki';
+$lang['Geotagged'] = 'Landupplýsingamerkt';
+$lang['Go to'] = 'Fara til';
+$lang['Latitude'] = 'Breiddargráða';
+$lang['Link to this page'] = 'Tengill á þessa síðu';
+$lang['Loading'] = 'Hleðsla';
+$lang['Longitude'] = 'Lengdargráða';
+$lang['Map'] = 'Kort';
+$lang['More photos near this location'] = 'Fleiri myndir nálægt þessum stað';
+$lang['Next'] = 'Næsta';
+$lang['Not geotagged'] = 'Ekki landupplýsingamerkt';
+$lang['Prev'] = 'Fyrri';
+$lang['Show all'] = 'Sýna allt';
+$lang['Viewing'] = 'Skoða';
+$lang['World map'] = 'Heimskort';
+$lang['displays %s on a map'] = 'birtir %s á korti';
+$lang['displays all photos on a map'] = 'birtir allar myndir á korti';
+$lang['go to %s'] = 'fara til %s';
+$lang['opens %s in Google Earth'] = 'opnar %s í Google Earth (verður að vera uppsett á tölvunni þinni)';
