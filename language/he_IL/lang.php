638734aa0d3b629351ec01ec2f57a570df344a1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:03:39 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Failed'] = 'נכשל';
+$lang['Geotag'] = 'גיאותייג';
+$lang['Geotagged'] = 'גיאותייג';
+$lang['Go to'] = 'לך אל';
+$lang['Latitude'] = 'רוחב גיאוגרפי';
+$lang['Link to this page'] = 'קישור לדף זה';
+$lang['Loading'] = 'טוען';
+$lang['Longitude'] = 'אורך גיאוגרפי';
+$lang['Map'] = 'מפה';
+$lang['More photos near this location'] = 'תמונות נוספות בקרבת מקום זה';
+$lang['Next'] = 'הבא';
+$lang['Not geotagged'] = 'לא גיאותייג';
+$lang['Prev'] = 'הקודם';
+$lang['Show all'] = 'הצג הכל';
+$lang['Viewing'] = 'צפה ב';
+$lang['World map'] = 'מפת עולם';
+$lang['displays %s on a map'] = 'מציג %s על מפה';
+$lang['displays all photos on a map'] = 'מציג את כל התמונות על מפה';
+$lang['go to %s'] = 'לך אל %s';
+$lang['opens %s in Google Earth'] = 'פותח %s ב-Google Earth (חייב להיות מותקן על המחשב שלך)';
+$lang['out of %d'] = 'מתוך %d';
+$lang['show all photos around this location'] = 'הצג את כל התמונות בקרבת מקום זה';
