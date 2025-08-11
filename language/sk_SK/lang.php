32176196a54c6e151ec04fa21bc0da91d790bfed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-11 09:54:50 +0200

    Update sk_SK, thanks to jose1711

--- language/sk_SK/lang.php
+++ language/sk_SK/lang.php
@@ -1,12 +1,12 @@
 <?php
 $lang['Map']='Mapa';
 
-$lang['displays %s on a map']='zobrazí %s na mape';
+$lang['displays %s on a map'] = 'Zobrazí %s na mape';
 $lang['opens %s in Google Earth']='otvorí %s v aplikácii Google Earth (musíte ju mať nainštalovanú v počítači)';
 
 $lang['Go to'] = 'Prejsť na';
 
-$lang['go to %s']='choď na %s';
+$lang['go to %s'] = 'Prejsť na %s';
 $lang['Viewing']='Prehliadanie';
 $lang['Link to this page']='Odkaz na túto stránku';
 $lang['Show all']='Zobraziť všetko';
@@ -19,7 +19,7 @@ $lang['Next']='Nasledujúce';
 $lang['out of %d']='z %d';
 
 $lang['More photos near this location'] = 'Viac fotografií blízko tejto lokality';
-$lang['displays all photos on a map'] = 'zobraziť všetky fotky na mape';
+$lang['displays all photos on a map'] = 'Zobraziť všetky fotky na mape';
 $lang['show all photos around this location'] = 'ukázať všetky fotky okolo tejto lokality';
 $lang['Geotag'] = 'Geografická značka';
 $lang['Geotagged'] = 'Geograficky označené';
