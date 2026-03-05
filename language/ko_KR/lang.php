db3411d5906bffed7ccc2f9c0dd6adaf6128e49a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-05 12:55:24 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['out of %d'] = '총 %d개 중';
+$lang['show all photos around this location'] = '이 위치 주변의 모든 사진 표시';
+$lang['Failed'] = '실패';
+$lang['Geotag'] = '지오태그';
+$lang['Geotagged'] = '지오태그 적용됨';
+$lang['Go to'] = '이동';
+$lang['Latitude'] = '위도';
+$lang['Link to this page'] = '이 페이지 링크';
+$lang['Loading'] = '로드 중';
+$lang['Longitude'] = '경도';
+$lang['Map'] = '지도';
+$lang['More photos near this location'] = '이 위치 근처의 더 많은 사진';
+$lang['Next'] = '다음';
+$lang['Not geotagged'] = '지오태그되지 않음';
+$lang['Prev'] = '이전';
+$lang['Show all'] = '모두 표시';
+$lang['Viewing'] = '보기';
+$lang['World map'] = '세계 지도';
+$lang['displays %s on a map'] = '%s를 지도에 표시';
+$lang['displays all photos on a map'] = '모든 사진을 지도에 표시';
+$lang['go to %s'] = '%s로 이동';
+$lang['opens %s in Google Earth'] = 'Google Earth에서 %s 열기 (컴퓨터에 설치되어 있어야 함)';
